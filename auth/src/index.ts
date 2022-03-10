f7edcaa265286f23cc2aab313cfb0739e09e93d5
rdisamir <8415771+gitsamirp@users.noreply.github.com>
2022-03-10 20:30:57 +0000

    setup ingress

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,11 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
+
+app.get('/api/users/currentuser', (req, res) => {
+    res.send("Hi There");
+});
+
 app.listen(3000, () => {
     console.log("listening on portsss 3000");
 });
