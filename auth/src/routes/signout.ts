b85cccf3bec16e8c351d1df62139aeee19714da0
rdisamir <8415771+gitsamirp@users.noreply.github.com>
2022-03-24 16:16:20 +0000

    signout

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-    res.send('Hi there');
+    req.session = null;
+
+    res.send({});
 });
 
 export { router as signoutRouter };
