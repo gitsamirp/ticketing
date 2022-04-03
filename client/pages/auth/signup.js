aba4fc7d2ce5a8bf0c09ec69f8f13cb7a1110f47
rdisamir <8415771+gitsamirp@users.noreply.github.com>
2022-04-03 16:34:35 +0100

    add a request hook for re-useability

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,20 +1,21 @@
 import { useState } from 'react';
 import axios from 'axios';
+import useRequest from '../../hooks/use-request';
 
 const signup = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState([]);
 
+    const { doRequest, errors } = useRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+            email, password
+        }
+    })
     const onSubmit = async event => {
         event.preventDefault();
-        try {
-            const response = await axios.post('/api/users/signup', {
-                email, password
-            });
-        } catch (err) {
-            setErrors(err.response.data.errors);
-        }
+        doRequest();
 
     }
 
@@ -30,14 +31,7 @@ const signup = () => {
                 <label>Password</label>
                 <input value={password} onChange={event => setPassword(event.target.value)} type="password" className="form-control" />
             </div>
-            {errors.length > 0 && <div className='alert alert-danger'>
-                <h4>Oh Dear</h4>
-                <ul className='my-0'>
-                    {errors.map(err => (
-                        <li key={err.message}>{err.message}</li>
-                    ))}
-                </ul>
-            </div>}
+            {errors}
             <button className="btn btn-primary">Sign Up</button>
         </form>
     );
