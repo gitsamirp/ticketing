4326812ad30a3b6bc320de35bf7f7f2649726a88
rdisamir <8415771+gitsamirp@users.noreply.github.com>
2022-05-12 16:00:09 +0100

    sort out sign out and headers

--- client/components/header.js
+++ client/components/header.js
@@ -1,17 +1,31 @@
 import Link from 'next/link';
 
 const header = ({ currentUser }) => {
-    return <nav className="navbar navbar-light bg-light">
-        <Link href="/">
-            <a className='navbar-brand'>Flight Tickets</a>
-        </Link>
+  const links = [
+    !currentUser && { label: 'Sign up', href: '/auth/signup' },
+    !currentUser && { label: 'Sign In', href: '/auth/signin' },
+    currentUser && { label: 'Sign Out', href: '/auth/signout' },
+  ]
+  .filter(linkConfig => linkConfig)
+  .map((label, href) => {
+    return <li key={href} className="nav-item">
+      <Link href={href}>
+        <a className='nav-link'>{label}</a>
+      </Link>
+    </li>
+  })
 
-        <div className='d-flex justify-content-end'>
-            <ul className='nav d-flex align-items-center'>
-                { currentUser ? 'Sign Out' : 'Sign In' }
-            </ul>
-        </div>
-    </nav>
+  return <nav className="navbar navbar-light bg-light">
+    <Link href="/">
+      <a className='navbar-brand'>Flight Tickets</a>
+    </Link>
+
+    <div className='d-flex justify-content-end'>
+      <ul className='nav d-flex align-items-center'>
+        {links}
+      </ul>
+    </div>
+  </nav>
 };
 
 
